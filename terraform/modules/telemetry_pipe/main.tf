64aaca0a875e6d4d26ac352ed240c7465406d3a1
Dinesh Kandoria <dineshkandoriabiz@gmail.com>
2026-04-30 20:40:59 +0530

    fix: grant s3:ListBucket permission to Lambda IAM role for awswrangler

--- terraform/modules/telemetry_pipe/main.tf
+++ terraform/modules/telemetry_pipe/main.tf
@@ -43,10 +43,14 @@ resource "aws_iam_role_policy" "lambda_s3_policy" {
       {
         Action = [
           "s3:GetObject",
-          "s3:PutObject"
+          "s3:PutObject",
+          "s3:ListBucket"
         ]
         Effect   = "Allow"
-        Resource = "${aws_s3_bucket.data_lake.arn}/*"
+        Resource = [
+          "${aws_s3_bucket.data_lake.arn}",
+          "${aws_s3_bucket.data_lake.arn}/*"
+        ]
       },
       {
         Action = [
